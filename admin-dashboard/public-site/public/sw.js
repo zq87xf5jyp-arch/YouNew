c96a1baf87e7d655ebfc719fca7c6ae0e043067e
zq87xf5jyp-arch <zq87xf5jyp@privaterelay.appleid.com>
2026-08-04 19:15:48 +0200

    Web: refresh release cache version

--- admin-dashboard/public-site/public/sw.js
+++ admin-dashboard/public-site/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "younew-web-188ee34e5ff9";
+const CACHE_VERSION = "younew-web-7d611bc32dd0";
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const GUIDE_CACHE = `${CACHE_VERSION}-guides`;
 const ASSET_CACHE = `${CACHE_VERSION}-assets`;
